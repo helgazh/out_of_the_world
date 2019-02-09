bf2127d8853b319276ab8fe04f979f1eef81759e
helgazh <helgazhadan@gmail.com>
2019-02-09 12:49:03 +0200

    Описание коммитаinit

---
+++ src/assets/js/app.js
@@ -0,0 +1,27 @@
+import $ from 'jquery';
+import 'what-input';
+
+// Foundation JS relies on a global varaible. In ES6, all imports are hoisted
+// to the top of the file so if we used`import` to import Foundation,
+// it would execute earlier than we have assigned the global variable.
+// This is why we have to use CommonJS require() here since it doesn't
+// have the hoisting behavior.
+window.jQuery = $;
+// require('foundation-sites');
+
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
+import './lib/foundation-explicit-pieces';
+import slick from 'slick-carousel/slick/slick.min.js';
+import 'lightbox2/dist/js/lightbox.js';
+
+$(document).foundation();
+
+$('.one-time').slick({
+    dots: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    arrows : false
+});
+
